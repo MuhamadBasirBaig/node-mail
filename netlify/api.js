ce177186234b5e96cb2564c8d728a945bbe4ee9f
Faizan Ejaz <mirzafaizanejaz@gmail.com>
2024-11-14 16:05:04 +0500

    update api.ts

--- netlify/api.js
+++ netlify/api.js
@@ -1,29 +1,32 @@
 import bodyParser from "body-parser";
 import express, { Router } from "express";
 import serverless from "serverless-http";
-
+// const express = require('express')
+// const app = express()
+// const initRoutes = require('../src/routes/web')
+// Khởi tạo các routes cho ứng dụng
+// initRoutes(app)
+const PORT = 3001;
+const router = Router();
 const api = express();
 api.use(bodyParser.json())
-const router = Router();
+
 
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
 api.use("/api/", router);
 
 
-// const express = require('express')
-// const app = express()
-// const initRoutes = require('../src/routes/web')
+
 
 // Cho phép lý dữ liệu từ form method POST
-app.use(express.urlencoded({extended: true}))
-
-// Khởi tạo các routes cho ứng dụng
-initRoutes(app)
+api.use(express.urlencoded({extended: true}))
 
 
-app.listen(port, () => {
-  console.log(`Hello trungquandev.com, I'm running at localhost:${port}/`)
-})
+
+
+// api.listen(PORT, () => {
+//   console.log(`Hello trungquandev.com, I'm running at localhost:${port}/`)
+// })
 
 export const handler = serverless(api);
