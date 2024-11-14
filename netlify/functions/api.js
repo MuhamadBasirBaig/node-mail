1d3bff3ff278a052280c8233e5860282cedb06f6
Faizan Ejaz <mirzafaizanejaz@gmail.com>
2024-11-14 17:08:03 +0500

    added email route

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,6 +1,8 @@
 import bodyParser from "body-parser";
 import express, { Router } from "express";
 import serverless from "serverless-http";
+const emailController = require('./src/controllers/emailController')
+
 // const express = require('express')
 // const app = express()
 // const initRoutes = require('../src/routes/web')
@@ -13,7 +15,7 @@ api.use(bodyParser.json())
 
 // Cho phép lý dữ liệu từ form method POST
 api.use(express.urlencoded({extended: true}))
-
+router.post('/send-email', emailController.sendMail)
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
 api.use("/api/", router);
